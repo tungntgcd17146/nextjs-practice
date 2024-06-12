c67eb69a1b5021f2e05eb4d224ce4ce24ada61d5
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-06-12 11:04:10 +0700

    fix(config): remove redundant

--- nextjs-practice/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, './');
-    return config;
-  },
-};
-
-export default nextConfig;
