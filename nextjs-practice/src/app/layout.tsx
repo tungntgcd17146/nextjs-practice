41a80e74eade0c803d2320fc6f04d6438def50ac
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-07-01 16:14:20 +0700

    fix: update redundant and add sample env

--- nextjs-practice/src/app/layout.tsx
+++ nextjs-practice/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={inter.className}>
         <StyledEngineProvider injectFirst>
           <ModeContextProvider>
             <CssBaseline />
