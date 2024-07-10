483a4d43fcefd608b47c8e03c7c2d7475ccf6178
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-07-10 17:56:15 +0700

    fix: next build

--- nextjs-practice/src/components/ui/SwitchMode/SwitchMode.stories.tsx
+++ nextjs-practice/src/components/ui/SwitchMode/SwitchMode.stories.tsx
@@ -20,7 +20,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const SwitchModeBase: Story = {
   args: {
-    isLargerDrawerOnTablet: false,
+    shouldIconButton: false,
     customWidth: '300px',
   },
 };
